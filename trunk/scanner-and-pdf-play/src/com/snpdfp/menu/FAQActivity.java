3d139003a90bd07a88c89d3b45ab60d35cb403bb
kumar.shikhar83@gmail.com <kumar.shikhar83@gmail.com@a686cf87-48b6-46c0-cdd0-e21fb29b6aa4>
2013-03-11 15:57:28 +0000

    New FAQ added

--- trunk/scanner-and-pdf-play/src/com/snpdfp/menu/FAQActivity.java
+++ trunk/scanner-and-pdf-play/src/com/snpdfp/menu/FAQActivity.java
@@ -19,6 +19,13 @@ public class FAQActivity extends SNPDFActivity {
 
 		TextView answer1 = (TextView) findViewById(R.id.answer1);
 		answer1.setText(SAPDFPathManager.getRootDirectory().getAbsolutePath());
+
+		TextView question2 = (TextView) findViewById(R.id.question2);
+		question2.setText("How can I add text to existing PDF?");
+
+		TextView answer2 = (TextView) findViewById(R.id.answer2);
+		answer2.setText("1. Prepare a .txt file with new text and convert this file to PDF using this tool.\n2. Finally concatenate this created PDF to the intended PDF.");
+
 	}
 
 }
