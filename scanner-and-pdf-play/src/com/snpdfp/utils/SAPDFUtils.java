15d86c97d7d6d74e35f990dde5f42d21c99528dc
kumar.shikhar83@gmail.com <kumar.shikhar83@gmail.com@a686cf87-48b6-46c0-cdd0-e21fb29b6aa4>
2013-03-17 15:41:11 +0000

    View Changes

--- scanner-and-pdf-play/src/com/snpdfp/utils/SAPDFUtils.java
+++ scanner-and-pdf-play/src/com/snpdfp/utils/SAPDFUtils.java
@@ -5,15 +5,23 @@ import android.widget.TextView;
 public class SAPDFUtils {
 
 	public static void setErrorText(TextView textView, String message) {
-		//textView.setBackgroundColor(-65536);
+		// textView.setBackgroundColor(-65536);
 		textView.setText(message);
 		textView.setTextColor(-65536);
 	}
 
 	public static void setSuccessText(TextView textView, String message) {
-		//textView.setBackgroundColor(-16711936);
+		// textView.setBackgroundColor(-16711936);
 		textView.setText(message);
 		textView.setTextColor(-16711936);
+	}
+
+	public static String getSizeText(long length) {
+		if (length < 1024)
+			return length + " B";
+		int exp = (int) (Math.log(length) / Math.log(1024));
+		String pre = "KMGTPE".charAt(exp - 1) + "";
+		return String.format("%.1f %sB", length / Math.pow(1024, exp), pre);
 	}
 
 }
---
+++ scanner-and-pdf-play/src/com/snpdfp/utils/SNPDFArrayAdapter.java
@@ -0,0 +1,50 @@
+package com.snpdfp.utils;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.snpdfp.activity.R;
+
+public class SNPDFArrayAdapter extends ArrayAdapter<File> {
+	private final Context context;
+	private final List<File> files;
+
+	public SNPDFArrayAdapter(Context context, List<File> files) {
+		super(context, R.layout.row_snpdf_view, files);
+		this.context = context;
+		this.files = files;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View rowView = inflater.inflate(R.layout.row_snpdf_view, parent, false);
+
+		TextView fileNameView = (TextView) rowView.findViewById(R.id.rowtext);
+		TextView fileDetailView = (TextView) rowView
+				.findViewById(R.id.rowdetail);
+
+		File file = files.get(position);
+		fileNameView.setText(file.getName());
+		fileDetailView
+				.setText(SAPDFUtils.getSizeText(file.length())
+						+ ", "
+						+ new SimpleDateFormat("MMM dd,yyyy HH:mm", Locale
+								.getDefault()).format(new Date(file
+								.lastModified())));
+
+		return rowView;
+	}
+
+}
