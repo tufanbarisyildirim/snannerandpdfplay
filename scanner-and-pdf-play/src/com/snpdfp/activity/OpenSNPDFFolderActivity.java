8621b1695a2b1fddae6645bcebce2cab96078581
kumar.shikhar83@gmail.com <kumar.shikhar83@gmail.com@a686cf87-48b6-46c0-cdd0-e21fb29b6aa4>
2013-07-27 09:50:59 +0000

    delete all modified

--- scanner-and-pdf-play/src/com/snpdfp/activity/OpenSNPDFFolderActivity.java
+++ scanner-and-pdf-play/src/com/snpdfp/activity/OpenSNPDFFolderActivity.java
@@ -8,7 +8,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,10 +15,8 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.snpdfp.utils.SNPDFArrayAdapter;
 import com.snpdfp.utils.SNPDFCContstants;
@@ -64,9 +61,6 @@ public class OpenSNPDFFolderActivity extends SNPDFActivity {
 				none.add("NONE");
 				lv.setAdapter(new ArrayAdapter<String>(this, R.layout.row, none));
 
-				Button button = (Button) findViewById(R.id.deleteAll);
-				button.setVisibility(View.GONE);
-
 			} else {
 				lv.setAdapter(new SNPDFArrayAdapter(this, files));
 
@@ -95,46 +89,6 @@ public class OpenSNPDFFolderActivity extends SNPDFActivity {
 		startActivity(filePick);
 	}
 
-	public void deleteAll(View view) {
-		getAlertDialog()
-				.setTitle("Delete all files?")
-				.setMessage("Are you sure to delete all prepared files?")
-				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.dismiss();
-						deleteAll();
-					}
-
-				})
-				.setNegativeButton("Cancel",
-						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog,
-									int which) {
-								dialog.dismiss();
-							}
-						}).show();
-	}
-
-	private void deleteAll() {
-		File snpdf = SNPDFPathManager.getRootDirectory();
-		File[] list = snpdf.listFiles();
-
-		if (list != null && list.length > 0) {
-			for (File file : list) {
-				file.delete();
-			}
-		}
-
-		Toast.makeText(this, "All snpdf prepared files deleted!",
-				Toast.LENGTH_SHORT).show();
-
-		Intent intent = new Intent(this, MainActivity.class);
-		intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-		startActivity(intent);
-	}
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
