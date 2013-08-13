6b80e3a00952aefac375a8ac61998206385e609e
kumar.shikhar83@gmail.com <kumar.shikhar83@gmail.com@a686cf87-48b6-46c0-cdd0-e21fb29b6aa4>
2013-08-13 14:55:07 +0000

    multi split added

--- scanner-and-pdf-play/src/com/snpdfp/utils/SNPDFUtils.java
+++ scanner-and-pdf-play/src/com/snpdfp/utils/SNPDFUtils.java
@@ -16,96 +16,98 @@ import com.snpdfp.activity.R;
 
 public class SNPDFUtils {
 
-	public static void setErrorText(Activity activity, String message) {
-		TextView textView = (TextView) activity.findViewById(R.id.message);
-		ImageView imageView = (ImageView) activity.findViewById(R.id.imageOP);
+  public static void setErrorText(Activity activity, String message) {
+    TextView textView = (TextView) activity.findViewById(R.id.message);
+    ImageView imageView = (ImageView) activity.findViewById(R.id.imageOP);
 
-		textView.setText(message);
-		textView.setTextColor(Color.parseColor("#FF0040"));
+    textView.setText(message);
+    textView.setTextColor(Color.parseColor("#FF0040"));
 
-		imageView.setImageResource(R.drawable.wrong);
+    imageView.setImageResource(R.drawable.wrong);
 
-	}
+  }
 
-	public static void setSuccessText(Activity activity, String message,
-			File file) {
-		TextView textView = (TextView) activity.findViewById(R.id.message);
-		ImageView imageView = (ImageView) activity.findViewById(R.id.imageOP);
+  public static void setSuccessText(Activity activity, String message) {
+    TextView textView = (TextView) activity.findViewById(R.id.message);
+    ImageView imageView = (ImageView) activity.findViewById(R.id.imageOP);
 
-		String fileDetails = "Filename:"
-				+ file.getName()
-				+ "\nSize:"
-				+ SNPDFUtils.getSizeText(file.length())
-				+ "\nDate Modified:"
-				+ new SimpleDateFormat(SNPDFCContstants.DATE_FORMAT,
-						Locale.getDefault()).format(new Date(file
-						.lastModified()));
-		textView.setText(message + "\n" + fileDetails);
-		textView.setTextColor(Color.parseColor("#298A08"));
+    textView.setText(message);
+    textView.setTextColor(Color.parseColor("#298A08"));
 
-		imageView.setImageResource(R.drawable.correct);
-	}
+    imageView.setImageResource(R.drawable.correct);
 
-	public static String getSizeText(long length) {
-		if (length < 1024)
-			return length + " B";
-		int exp = (int) (Math.log(length) / Math.log(1024));
-		String pre = "KMGTPE".charAt(exp - 1) + "";
-		return String.format("%.1f %sB", length / Math.pow(1024, exp), pre);
-	}
+  }
 
-	public static void setErrorText(TextView textView, String message) {
-		textView.setText(message);
-		textView.setTextColor(Color.parseColor("#FF0040"));
-	}
+  public static void setSuccessText(Activity activity, String message, File file) {
+    TextView textView = (TextView) activity.findViewById(R.id.message);
+    ImageView imageView = (ImageView) activity.findViewById(R.id.imageOP);
 
-	public static boolean isPDFPasswordCorrect(boolean password_req,
-			String password, File file) {
-		if (password_req) {
-			if (password == null || password.equals("")
-					|| !isPasswordCorrect(file, password)) {
-				return false;
-			}
-		}
+    String fileDetails = "Filename:" + file.getName() + "\nSize:" + SNPDFUtils.getSizeText(file.length())
+        + "\nDate Modified:"
+        + new SimpleDateFormat(SNPDFCContstants.DATE_FORMAT, Locale.getDefault()).format(new Date(file.lastModified()));
+    textView.setText(message + "\n" + fileDetails);
+    textView.setTextColor(Color.parseColor("#298A08"));
 
-		return true;
-	}
+    imageView.setImageResource(R.drawable.correct);
+  }
 
-	public static boolean isProtected(File srcFile) {
-		PdfReader pdfReader = null;
-		boolean encrypted = false;
-		try {
-			pdfReader = new PdfReader(srcFile.getAbsolutePath());
-			if (pdfReader.isEncrypted()) {
-				encrypted = true;
-			}
+  public static String getSizeText(long length) {
+    if (length < 1024)
+      return length + " B";
+    int exp = (int) (Math.log(length) / Math.log(1024));
+    String pre = "KMGTPE".charAt(exp - 1) + "";
+    return String.format("%.1f %sB", length / Math.pow(1024, exp), pre);
+  }
 
-		} catch (IOException e) {
-			encrypted = true;
-		} finally {
-			if (pdfReader != null) {
-				pdfReader.close();
-			}
-		}
+  public static void setErrorText(TextView textView, String message) {
+    textView.setText(message);
+    textView.setTextColor(Color.parseColor("#FF0040"));
+  }
 
-		return encrypted;
-	}
+  public static boolean isPDFPasswordCorrect(boolean password_req, String password, File file) {
+    if (password_req) {
+      if (password == null || password.equals("") || !isPasswordCorrect(file, password)) {
+        return false;
+      }
+    }
 
-	public static boolean isPasswordCorrect(File srcFile, String password) {
-		PdfReader pdfReader = null;
-		try {
-			pdfReader = new PdfReader(srcFile.getAbsolutePath(),
-					password.getBytes());
-			return true;
-		} catch (IOException e) {
+    return true;
+  }
 
-		} finally {
-			if (pdfReader != null) {
-				pdfReader.close();
-			}
-		}
+  public static boolean isProtected(File srcFile) {
+    PdfReader pdfReader = null;
+    boolean encrypted = false;
+    try {
+      pdfReader = new PdfReader(srcFile.getAbsolutePath());
+      if (pdfReader.isEncrypted()) {
+        encrypted = true;
+      }
 
-		return false;
-	}
+    } catch (IOException e) {
+      encrypted = true;
+    } finally {
+      if (pdfReader != null) {
+        pdfReader.close();
+      }
+    }
+
+    return encrypted;
+  }
+
+  public static boolean isPasswordCorrect(File srcFile, String password) {
+    PdfReader pdfReader = null;
+    try {
+      pdfReader = new PdfReader(srcFile.getAbsolutePath(), password.getBytes());
+      return true;
+    } catch (IOException e) {
+
+    } finally {
+      if (pdfReader != null) {
+        pdfReader.close();
+      }
+    }
+
+    return false;
+  }
 
 }
